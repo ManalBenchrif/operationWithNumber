eafa4afa04715c26505129f7633107e0d33d1d64
Manal BENCHRIF <72361536+ManalBenchrif@users.noreply.github.com>
2020-10-20 01:57:25 +0100

    Add files via upload

---
+++ operationNumber.py
@@ -0,0 +1,13 @@
+#for numbers we can use common operations : +,-,*,/,%
+#Just as any other programming languages, the addition, subtraction, multiplication, and division operators can be used with numbers.
+#Does python follow order of operations?= yessss
+x=20
+y=14
+substraction=x-y
+addition=x+y
+multiplication=x*y
+division=x/y
+power=x**2
+remainder=x%y #dividend % divisor = remainder.
+print("x+y =",addition)
+print("x^2=",power)
